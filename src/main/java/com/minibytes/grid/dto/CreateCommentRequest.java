5a9fb1038f5731a83e5661df3943762c54a83fd2
naveen-kerla <kerlanaveen19@gmail.com>
2026-04-20 11:05:07 +0530

    fix: lombok's @Builder not providing default constructor that jackson needs for JSON deserialization
    - add @noArfesconstructor, @AllArgesConstructor to fix

--- src/main/java/com/minibytes/grid/dto/CreateCommentRequest.java
+++ src/main/java/com/minibytes/grid/dto/CreateCommentRequest.java
@@ -2,9 +2,13 @@ package com.minibytes.grid.dto;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateCommentRequest {
     private Long authorId;
     private String authorType;
--- src/main/java/com/minibytes/grid/dto/CreateUserRequest.java
+++ src/main/java/com/minibytes/grid/dto/CreateUserRequest.java
@@ -2,9 +2,13 @@ package com.minibytes.grid.dto;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateUserRequest {
     private String username;
     private boolean isPremium;
