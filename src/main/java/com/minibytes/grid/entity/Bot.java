07a1bcf26b78132db058b2a854af6174b3c05508
naveen-kerla <kerlanaveen19@gmail.com>
2026-04-20 10:23:19 +0530

    chore: apply Lombok annotations to Bot and User entities

--- src/main/java/com/minibytes/grid/entity/Bot.java
+++ src/main/java/com/minibytes/grid/entity/Bot.java
@@ -3,8 +3,16 @@ package com.minibytes.grid.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.Builder;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Bot {
     @Id
     @GeneratedValue
--- src/main/java/com/minibytes/grid/entity/User.java
+++ src/main/java/com/minibytes/grid/entity/User.java
@@ -3,8 +3,16 @@ package com.minibytes.grid.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
+import lombok.Data;
+import lombok.Builder;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     @Id @GeneratedValue
     Long id;
