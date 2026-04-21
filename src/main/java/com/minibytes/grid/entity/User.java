2cec9ac7b0226c418db9387750f8c337cd825289
naveen-kerla <kerlanaveen19@gmail.com>
2026-04-22 00:47:58 +0530

    fix: fix the postgresQL migration issues
    - fix the versions of postgreSQL and redis
    - fix the user entity to users, since the user is keyword in postgresSql

--- src/main/java/com/minibytes/grid/entity/User.java
+++ src/main/java/com/minibytes/grid/entity/User.java
@@ -3,12 +3,14 @@ package com.minibytes.grid.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.Builder;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "users")
 @Data
 @Builder
 @AllArgsConstructor
